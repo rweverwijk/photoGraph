b63725826fe01fb730c6807dda8d6e4581fb6fb3
Ron van Weverwijk <rvanweverwijk@xebia.com>
2014-04-11 20:16:32 +0200

    moved logic to thumb module. And implemented creation of subdirectories.

--- server.js
+++ server.js
@@ -1,22 +1,10 @@
-var gm = require('gm');
 var fs = require('fs');
 var express = require('express');
-var _str = require("underscore.string");
-//var thumb = require("./thumb.js");
+var thumb = require("./thumb.js");
+var path = require('path');
 
 var app = express();
-var staticMiddleware = express.static(__dirname + '/public');
-
-var flickrSize = {
-  s: {width: 75, height: 75, option: "!"},
-  q: {width: 150, height: 150, option: "!"},
-  t: {width: 100, height: 100, option: undefined},
-  m: {width: 240, height: 240, option: undefined},
-  n: {width: 320, height: 320, option: undefined},
-  z: {width: 640, height: 640, option: undefined},
-  c: {width: 800, height: 800, option: undefined},
-  b: {width: 1024, height: 1024, option: undefined}
-};
+var staticMiddleware = express.static(path.join(__dirname + '/thumbnail'));
 
 app.get('/', function(req, res){
   res.send('hello world');
@@ -24,41 +12,24 @@ app.get('/', function(req, res){
 
 app.use('/static', express.static(__dirname + '/static'));
 
-app.get('/public/:file', function(req, res, next){
+app.get('/thumbnail/:file*', function(req, res, next){
+    var fileName = req.params.file + (req.params[0] ? req.params[0] : "");
     console.log('about to send restricted file '+ req.params.file);
-    var fileName = req.params.file;
-    fs.exists("./public/" + fileName, function(exists) {
+    console.log('about to send restricted file '+ JSON.stringify(req.params));
+    var serveFile = function() {
+      req.url = req.url.replace(/^\/thumbnail/, '');
+      staticMiddleware(req, res, next);
+    };
+    
+    console.log(fileName);
+    fs.exists("./thumbnail/" + fileName, function(exists) {
       if (!exists) {
-        console.log('generating thumbail for: ' + fileName);
-        var fSize = getFlickrSize(fileName);
-        gm(getOriginalFileLocation(fileName))
-        .resize(fSize.width, fSize.height, fSize.option)
-        .write('/Users/rvanweverwijk/projects/rweverwijk/photonodetest/public/' + fileName, function (err) {
-          if (!err) {
-            req.url = req.url.replace(/^\/public/, '');
-            staticMiddleware(req, res, next);
-          }
-          if (err) console.error(err);
-        });
+        thumb.generateThumbnail(fileName, serveFile);
       } else {
-        req.url = req.url.replace(/^\/public/, '');
-        staticMiddleware(req, res, next);
+        serveFile();
       }
     });
     
 });
-
-var getFlickrSize = function(fileName) {
-  if (fileName.substr(fileName.length - 6, 1) == "_") {
-    return flickrSize[fileName.substr(fileName.length - 5, 1)];  
-  } else {
-    return {width: 500, height: 500};
-  }
-};
-
-var getOriginalFileLocation = function(fileName) {
-  var f = fileName.substr(fileName.length - 6, 1) == "_" ? fileName.substr(0, fileName.length - 6) + ".jpg" : fileName;
-  return '/Users/rvanweverwijk/projects/rweverwijk/photonodetest/original/' + f;
-};
 
 app.listen(3000);
