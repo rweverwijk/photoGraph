6be12bbc2b38935626f2a2f81822974f9dd9d0d8
Ron van Weverwijk <rvanweverwijk@xebia.com>
2014-04-23 07:38:52 +0200

    added tags to the rest service

--- photo.js
+++ photo.js
@@ -1,22 +1,31 @@
+var _ = require('underscore');
 var neo4j = require("neo4j");
 var db = new neo4j.GraphDatabase('http://localhost:7474');
 
 exports.getRandomPhotos = function(callback) {
   var query = [
   'MATCH (p:Photo)',
-  'RETURN p',
-  'LIMIT 10'
+  'OPTIONAL MATCH (p)-[:HAS_TAG]->(t)',
+  'WITH p, collect(t.name) as t',
+  'WITH p,t, rand() as random',
+  'RETURN p.fileName as fileName, p.directory as directory, t as tags',
+  'ORDER BY random',
+  'LIMIT 50'
   ].join('\n');
 
   // var params = {
   //   userId: currentUser.id
   // };
   db.query(query, {}, function (err, results) {
-    console.log("p" + results);
+    console.log("p" + JSON.stringify(results));
     if (err) throw err;
-    var photos = results.map(function (result) {
-      return result.p._data.data;
+    var photos = results;
+    
+    _.each(photos, function(photo) {
+      photo.thumbnailUrl = photo.fileName.replace(".JPG", "_t.JPG").replace(".jpg", "_t.jpg");
+      photo.fileName = photo.fileName.replace(".JPG", "_b.JPG").replace(".jpg", "_b.jpg");
     });
+
     callback(photos);
   });
 };
