77e370c48f7cc4ee8629c4771c9317e27e181c03
Ron van Weverwijk <rvanweverwijk@xebia.com>
2014-04-23 22:13:56 +0200

    added a watch to the directive to reload on change of photos

--- static/controllers.js
+++ static/controllers.js
@@ -1,10 +1,22 @@
 var galleryApp = angular.module('galleryApp', []);
 
 galleryApp.controller('PhotoListCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
-  var tag = $location.search().tag;
-  var url = '/photo/' + (tag ? '?tag=' + tag : '');
-  $http.get(url).success(function(data) {
-    $scope.phones = data;
-  });
+  $scope.fetchPhotos = function() {
+    var tag = _.filter($scope.tags, function(item) {return item.selected;});
+    var photoUrl = '/photo/' + (tag.length > 0 ? '?tag=' + tag[0].name : '');
+    $http.get(photoUrl).success(function(data) {
+      $scope.phones = data;
+    });  
+  };
+  
+  $scope.fetchTags = function() {
+    $http.get('/tags/').success(function(data) {
+      $scope.tags = data;
+    });  
+  };
+
+  $scope.fetchTags();
+  $scope.fetchPhotos();
+  
 }]);
 
