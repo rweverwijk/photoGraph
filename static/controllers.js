84b91137952726ff0fa50a58f7a5d389e040852e
Ron van Weverwijk <rvanweverwijk@xebia.com>
2014-04-24 19:24:22 +0200

    added toggle for random photo's

--- static/controllers.js
+++ static/controllers.js
@@ -3,7 +3,7 @@ var galleryApp = angular.module('galleryApp', []);
 galleryApp.controller('PhotoListCtrl', ['$scope', '$http', '$location', function ($scope, $http, $location) {
   $scope.fetchPhotos = function() {
     var tagNames = _.map(_.filter($scope.tags, function(item) {return item.selected;}), function(item) {return item.name;});
-    var photoUrl = '/photo/?order=random' + (tagNames.length > 0 ? '&tag=' + tagNames.join("&tag=") : '');
+    var photoUrl = '/photo/?order=' + ($scope.random ? "random" : "file") + (tagNames.length > 0 ? '&tag=' + tagNames.join("&tag=") : '');
     $http.get(photoUrl).success(function(data) {
       $scope.phones = data;
     });  
