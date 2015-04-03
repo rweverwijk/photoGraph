02b2541732bcd873b23941b92c7e44027373ccad
Ron van Weverwijk <rvanweverwijk@xebia.com>
2015-04-03 16:40:26 +0200

    added a python implementation

---
+++ python/server.py
@@ -0,0 +1,57 @@
+import os
+from flask import Flask, send_from_directory
+from py2neo import Graph, Node, Relationship,watch
+import json
+
+app = Flask(__name__)
+
+# watch("httpstream")
+graph = Graph('http://neo4j:test@localhost:7474/db/data')
+
+graph.cypher.execute('MATCH (t:Tag) RETURN t ORDER BY t.name')
+
+def get_tags():
+    query_result = graph.cypher.execute('MATCH (t:Tag) RETURN t ORDER BY t.name')
+    tags = []
+    for record in query_result:
+        tags.append({'name': record.t['name']})
+    return json.dumps(tags)
+
+def get_photos():
+  query_partial = [
+    'MATCH (p:Photo)',
+    'WITH p',
+    'MATCH (p)-[:HAS_TAG]->(t)',
+    'WITH p, collect(t.name) as t',
+    'WITH p,t, rand() as random',
+    'RETURN p.fileName as fileName, p.directory as directory, t as tags',
+    'LIMIT 90'
+  ];
+  query_result = graph.cypher.execute('\n'.join(query_partial))
+  photos = []
+
+  for record in query_result:
+    photos.append(transformImageName({'fileName': record.fileName, 'directory': record.directory, 'tags': record.tags}))
+
+  return json.dumps(photos)
+
+def transformImageName(photo):
+  photo['thumbnailUrl'] = photo['fileName'].replace(".JPG", "_t.JPG").replace(".jpg", "_t.jpg");
+  photo['fileName'] = photo['fileName'].replace(".JPG", "_b.JPG").replace(".jpg", "_b.jpg");
+  return photo
+
+@app.route('/tags')
+def tags():
+    return get_tags()
+
+@app.route('/photo')
+def photos():
+    return get_photos()
+
+@app.route('/thumbnail/<path:path>')
+def thumbnail(path):
+  print('get thumbnail for: ' + path)
+  return send_from_directory('../nodejs/thumbnail', path)
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/controllers.js
+++ static/controllers.js
@@ -14,14 +14,14 @@ galleryApp.controller('PhotoListCtrl', ['$scope', '$http', '$location', function
   $scope.fetchPhotos = function() {
     var tagNames = _.map(_.filter($scope.tags, function(item) {return item.selected;}), function(item) {return item.name;});    
 
-    var photoUrl = '/photo/?order=' + ($scope.random ? "random" : "file") + (tagNames.length > 0 ? '&tag=' + tagNames.join("&tag=") : '');
+    var photoUrl = '/photo?order=' + ($scope.random ? "random" : "file") + (tagNames.length > 0 ? '&tag=' + tagNames.join("&tag=") : '');
     $http.get(photoUrl).success(function(data) {
       $scope.photos = data;
     });  
   };
   
   $scope.fetchTags = function() {
-    $http.get('/tags/').success(function(data) {
+    $http.get('/tags').success(function(data) {
       var tags = [].concat($location.search().tag);
       _.each(data, function(item) {
         if (_.contains(tags, item.name)) {
